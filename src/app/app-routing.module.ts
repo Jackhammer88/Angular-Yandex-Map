28542a65f26e4db5a3ecdb652cc04829957ae78f
Alexey Morkovin <madfisht3@gmail.com>
2021-12-05 02:42:13 +0300

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', loadChildren: ()=> import("./modules/greet/greet.module").then(m => m.GreetModule)},
+  {path: 'my-map', loadChildren: ()=> import("./modules/my-map/my-map.module").then(m => m.MyMapModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
